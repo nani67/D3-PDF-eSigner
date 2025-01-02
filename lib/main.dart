7c4744e9302e7612be28f5bcaf64560818b497d0
Penumala Nani <44634884+nani67@users.noreply.github.com>
2025-01-02 18:48:17 +0530

    Theming information added for flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'configs.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp, DeviceOrientation.portraitDown, DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight
   ]);
-  runApp(MyApp());
+  runApp(MyAppStateless());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppStateless extends StatelessWidget {
+  const MyAppStateless({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +24,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Configs.mainColor,
       ),
-      home: Scaffold(
+      home: MyAppStateful(),
+    );
+  }
+}
+
+class MyAppStateful extends StatefulWidget {
+  const MyAppStateful({super.key});
+
+  @override
+  State<MyAppStateful> createState() => MyApp();
+}
+
+class MyApp extends State<MyAppStateful> {
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    await Future.delayed(Duration(seconds: 1));
+    FlutterNativeSplash.remove();
+    // await Firebase.initializeApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Configs.mainColor,
           title: Text('PDF eSigner'),
@@ -43,7 +74,6 @@ class MyApp extends StatelessWidget {
           ),
             ),
         ),
-        )
       ),
     );
   }
